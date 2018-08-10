2bd86015c76cb957ae1e6a3a4d3514364125a8fc
Zavier <zmyles@comcast.net>
2018-08-09 23:34:42 -0400

    Styles in places

---
+++ basic/scripts/ctrl.js
@@ -0,0 +1,33 @@
+
+
+function TodoController() {
+  const vm = this;
+  vm.toDoList = [ {
+    task: "Walk the dog.",
+    completed: false
+  },
+
+  {
+    task: "Go to the store.",
+    completed: true
+  },
+
+  {
+    task: "Get gas.",
+    completed: false
+  },
+
+  {
+    task: "Wash the car",
+    completed: false
+  }
+
+  ]
+}
+
+
+{
+angular
+    .module("ToDoApp")
+    .controller("TodoController", TodoController);
+}
